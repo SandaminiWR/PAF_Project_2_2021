f796efc37d8d7fd981a2702675335843a5cce103
Pasindu Nimsara <pasindunimsara26@gmail.com>
2021-05-10 20:54:59 +0530

    create API class

--- src/main/java/com/BuyerAPI.java
+++ src/main/java/com/BuyerAPI.java
@@ -1,8 +1,18 @@
 package com;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 
-@webServlet("/BuyerAPI")
-public class BuyerAPI {
+@WebServlet("/BuyerAPI")
+public class BuyerAPI extends HttpServlet{
+	private static final long serialVersionUID = 1L;
+    
+	Byer itemObj = new Byer();
+	
+    public BuyerAPI() {
+        super();
+        
+    }
 	
 	
 
