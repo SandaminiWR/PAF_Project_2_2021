cbf3199fe30f595e317bc9790f8fdf7f8c3afb94
Pasindu Nimsara <pasindunimsara26@gmail.com>
2021-05-10 21:47:24 +0530

    create program

--- src/main/java/com/Byer.java
+++ src/main/java/com/Byer.java
@@ -1,106 +1,116 @@
 package com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.sql.*;
 
 public class Byer {
 	
 	//A common method to connect to the DB
-	public Connection connect() {
-		
+public Connection connect(){
+			
 		Connection con = null;
 		
 		try{
-		 Class.forName("com.mysql.jdbc.Driver");
+			Class.forName("com.mysql.jdbc.Driver");
 		 
-		//Provide the correct details: DBServer/DBName
+			//Provide the correct details: DBServer/DBName
 		 
-		con= DriverManager.getConnection("jdbc:mysql://localhost:3306/paf", "root", "");
-		  
+			con= DriverManager.getConnection("jdbc:mysql://localhost:3306/paf", "root", "");
+			System.out.print("Successfully Connected");
 			
-		}
-		catch (Exception e) {
-			e.printStackTrace();
+			}catch (Exception e) {
+				e.printStackTrace();
 			}
 		 
-		return con;
+			return con;
 
 }
 	//insert
-	public String insertbuyerservice(String FullName, String PhoneNumber, String Email, String Address, String Birthdate)
-	 {
-		String output = "";
-		 try{
-		 Connection con = connect();
-		 
-		 if (con == null){
-			 return "Error while connecting to the database for inserting."; 
-		 }
-		// create a prepared statement
-		 String query = " INSERT INTO `buyerservice`(`ID`, `FullName`, `PhoneNumber`, `Email`, `Address`, `Birthdate`) VALUES (?,?,?,?,?,?)";
-		 
-		 
-		 PreparedStatement preparedStmt = con.prepareStatement(query);
-		 
-		// binding values
-		 
-		 preparedStmt.setInt(1, 0);
-		 preparedStmt.setString(2, FullName);
-		 preparedStmt.setString(3, PhoneNumber);
-		 preparedStmt.setString(4, Email);
-		 preparedStmt.setString(5, Address); 
-		 preparedStmt.setString(6, Birthdate);
-		 
-		// execute the statement
-		 preparedStmt.execute();
-		 con.close();
-		 output = "Inserted successfully";
-		 }
-		 
-		 catch (Exception e){
-		 output = "Error while inserting the Details.";
-		 System.err.println(e.getMessage());
-		 }
-		 
-		 return output;
-		 
-		 }
+public String insertbuyerservice(String FullName, String PhoneNumber, String Email, String Address, String Birthdate){
 	
-	public String readbuyerservice() {
-	 String output = "";
+		String output = "";
+		
+		 try{
+			 Connection con = connect();
+			 
+			 if (con == null){
+				 return "Error while connecting to the database for inserting."; 
+			 }
+			 // create a prepared statement
+			 String query = " INSERT INTO `buyerservice`(`ID`, `FullName`, `PhoneNumber`, `Email`, `Address`, `Birthdate`) VALUES (?,?,?,?,?,?)";
+		 
+		 
+			 PreparedStatement preparedStmt = con.prepareStatement(query);
+		 
+			 // binding values
+		 
+			 preparedStmt.setInt(1, 0);
+			 preparedStmt.setString(2, FullName);
+			 preparedStmt.setString(3, PhoneNumber);
+			 preparedStmt.setString(4, Email);
+			 preparedStmt.setString(5, Address); 
+			 preparedStmt.setString(6, Birthdate);
+		 
+			 // execute the statement
+			 preparedStmt.execute();
+			 con.close();
+			 //output = "Inserted successfully";
+			 
+			 String newbuyer = readbuyerservice(); 
+			 output = "{\"status\":\"success\", \"data\": \"" + newbuyer + "\"}"; 
+		 	 
+		 }catch (Exception e){
+			 //output = "Error while inserting the Details.";
+			 output = "{\"status\":\"error\", \"data\": \"Error while inserting the Details.\"}";
+			 System.err.println(e.getMessage());
+		  }
+		 
+		  return output;
+		 
+}
+	
+public String readbuyerservice(){
+	
+		
+		String output = "";
 	 
 	 try{
-	 Connection con = connect();
-	 if (con == null){
-		 return "Error while connecting to the database for reading.";
+				 
+		 Connection con = connect();
+		 if (con == null){
+			 return "Error while connecting to the database for reading.";
 		 }
-	// Prepare the html table to be displayed
+		 // Prepare the html table to be displayed
 	 
-	 output = "<table border='1'><tr><th>Full_Name</th>"
-	 		+ "<th>PhoneNumber</th>"
-	 		+ "<th>Email</th>"
-	 		+ "<th>Address</th>"
-	 		+ "<th>Birthdate</th>"
-	 		+" <th>Update</th>"
-	 		+ "<th>Delete</th></tr>";
+		 output = "<table border='1'><tr><th>Full_Name</th>"
+				 + "<th>PhoneNumber</th>"
+				 + "<th>Email</th>"
+				 + "<th>Address</th>"
+				 + "<th>Birthdate</th>"
+				 +" <th>Update</th>"
+				 + "<th>Delete</th></tr>";
 	 
-	 String query = "select * from buyerservice";
-	 Statement stmt = con.createStatement();
-	 ResultSet rs = stmt.executeQuery(query);
+		 String query = "select * from buyerservice";
+		 Statement stmt = con.createStatement();
+		 ResultSet rs = stmt.executeQuery(query);
 	 
-	// iterate through the rows in the result set
-	 while (rs.next()) {
-		 String ID = Integer.toString(rs.getInt("ID"));
-	 	 String FullName = rs.getString("FullName");
-	 	 String PhoneNumber = rs.getString("PhoneNumber");
-	 	 String Email = rs.getString("Email");
-	 	 String Address = rs.getString("Address");
-	 	 String Birthdate = rs.getString("Birthdate");
+		 // iterate through the rows in the result set
+		 while (rs.next()){
+			 String ID = Integer.toString(rs.getInt("ID"));
+			 String FullName = rs.getString("FullName");
+			 String PhoneNumber = rs.getString("PhoneNumber");
+			 String Email = rs.getString("Email");
+			 String Address = rs.getString("Address");
+			 String Birthdate = rs.getString("Birthdate");
 	 	 
 	 	 
 	 	// Add into the html table
-	 	output += "<tr><td><input id='hidbuyerserviceupdate'
-	 			 name='hidbuyerserviceupdate' 
-	 			 type='hidden' value='" + ID + "'>"
+	 	output += "<tr><td><input id='hidbuyerserviceupdate'name='hidbuyerserviceupdate' type='hidden' value='" + ID + "'>";
 	 	 output += "<td>" + FullName + "</td>";
 	 	 output += "<td>" + PhoneNumber + "</td>";
 	 	 output += "<td>" + Email + "</td>"; 
@@ -127,52 +137,53 @@ public class Byer {
 	}
 	return output;
 		 
-	}
+}
 
 	//update
-	public String updatebuyerservice(String ID, String FullName, String PhoneNumber, String Email, String Address, String Birthdate) 
-	{
+public String updatebuyerservice(String ID, String FullName, String PhoneNumber, String Email, String Address, String Birthdate){
 	 
 	 String output = "";
 	 try{
-	 Connection con = connect();
-	 if (con == null){
-		 return "Error while connecting to the database for updating."; 
+		 Connection con = connect();
+		 if (con == null){
+			 return "Error while connecting to the database for updating."; 
 		 }
 	 
-	// create a prepared statement
-	 String query = "UPDATE buyerservice SET FullName =?, PhoneNumber=?, Email=?, Address =?, Birthdate =? WHERE ID= ?";
-	 // String query = "UPDATE buyerservice SET FullName=?,PhoneNumber=?,Email=?,Address=?,Birthdate=?  WHERE ID=?";
+		 // create a prepared statement
+		 String query = "UPDATE buyerservice SET FullName =?, PhoneNumber=?, Email=?, Address =?, Birthdate =? WHERE ID= ?";
+		 // String query = "UPDATE buyerservice SET FullName=?,PhoneNumber=?,Email=?,Address=?,Birthdate=?  WHERE ID=?";
 			 		
-		PreparedStatement preparedStmt = con.prepareStatement(query);
+		 PreparedStatement preparedStmt = con.prepareStatement(query);
 	 
-	// binding values
-	 preparedStmt.setString(1, FullName);
-	 preparedStmt.setString(2, PhoneNumber);
-	 preparedStmt.setString(3, Email);
-	 preparedStmt.setString(4, Address);
-	 preparedStmt.setString(5, Birthdate);
-	 preparedStmt.setInt(6, Integer.parseInt(ID));	
+		 // binding values
+		 preparedStmt.setString(1, FullName);
+		 preparedStmt.setString(2, PhoneNumber);
+		 preparedStmt.setString(3, Email);
+		 preparedStmt.setString(4, Address);
+		 preparedStmt.setString(5, Birthdate);
+		 preparedStmt.setInt(6, Integer.parseInt(ID));	
 	 
 	 
-	// execute the statement
-	 preparedStmt.execute();
-	 con.close();
-	 output = "Updated successfully";
-	 }
-	 
-	catch (Exception e){
+		 // execute the statement
+		 preparedStmt.execute();
+		 con.close();
+		 
+		 String newbuyer = readbuyerservice();
+		 output = "{\"status\":\"success\", \"data\": \"" + newbuyer + "\"}";
+		 //output = "Updated successfully";
+	 		
+	 }catch (Exception e){
 		
-	 output = "Error while updating the Details.";
-	 System.err.println(e.getMessage());
-	 
+		 //output = "Error while updating the Details.";
+		 output = "{\"status\":\"error\", \"data\": \"Error while updating the Details.\"}";
+		 System.err.println(e.getMessage());
 	 }
 	 
 	return output;
-	}
+}
 	
 	//delete
-	public String deletebuyerservice(String ID){
+public String deletebuyerservice(String ID){
 
 	String output = "";
 
@@ -192,17 +203,18 @@ public class Byer {
 		 // execute the statement
 		 preparedStmt.execute();
 		 con.close();
-		 output = "Deleted successfully";
-		  
+		 //output = "Deleted successfully";
+		 String newbuyer = readbuyerservice();
+		 output = "{\"status\":\"success\", \"data\": \"" + newbuyer + "\"}";
 
 		
+	}catch(Exception e){
+		
+		//output = "Error while deleting the Details.";
+		output = "{\"status\":\"error\", \"data\": \"Error while deleting the Details.\"}"; 
+		System.err.println(e.getMessage()); 
+
 	}
-	catch(Exception e){
-		
-		output = "Error while deleting the Details.";
-		 System.err.println(e.getMessage()); 
-
-}
 	return output;
-	}
+}
 }
