baf9ce92b537968bf58e52c76a673c7118612721
Pasindu Nimsara <pasindunimsara26@gmail.com>
2021-05-10 23:33:28 +0530

    edit code

--- webcontent/components/Buyers.js
+++ webcontent/components/Buyers.js
@@ -1,9 +1,10 @@
+
 $(document).ready(function(){
 	
-	if ($("#alertSuccess").text().trim() == "") {
+	//if ($("#alertSuccess").text().trim() == "") {
 		 $("#alertSuccess").hide(); 
- 	} 
- 	$("#alertError").hide(); 
+ 	//} 
+ 		 $("#alertError").hide(); 
 });
 
 // SAVE ============================================
@@ -19,52 +20,164 @@ $(document).on("click", "#btnSave", function(event){
  	 // Form validation-------------------
 	var status = validateItemForm(); 
 	if (status != true){ 
+ 		
  		$("#alertError").text(status); 
  		$("#alertError").show(); 
+		
 		 return; 
 	 } 
 	
 	// If valid-------------------------
- 		$("#buyerdetails").submit(); 
-});
+ 		//$("#buyerdetails").submit();
+ 		
+ 		var type = ($("#hidItemIDSave").val() == "") ? "POST" : "PUT"; 
+ 		
+		 $.ajax( 
+	     { 
+				 url : "BuyersAPI", 
+ 				 type : type, 
+                 data : $("#buyerdetails").serialize(), 
+                 dataType : "text", 
+                 complete : function(response, status) 
+ 				 {
+ 				 	onItemSaveComplete(response.responseText, status); 
+ 				 } 
+ 		}); 
+  }); 
+
+
+//Save Button View (Response Algorithm)--------------------------------------------------------------------------------------
+
+		function onItemSaveComplete(response, status){ 
+ 
+ 			if (status == "success"){ 
+ 	   		 	var resultSet = JSON.parse(response); 
+ 	   	 
+			 	if (resultSet.status.trim() == "success"){ 
+ 
+ 					$("#alertSuccess").text("Successfully saved."); 
+ 					$("#alertSuccess").show(); 
+ 					$("#divItemsGrid").html(resultSet.data);
+ 	 	 	
+ 	 	 	} 
+ 	 	 	else if (resultSet.status.trim() == "error"){ 
+ 	  			
+ 				$("#alertError").text(resultSet.data); 
+ 				$("#alertError").show(); 
+		    } 
+	 
+ 	    } 
+ 	    else if (status == "error"){
+   	     	
+ 			    $("#alertError").text("Error while saving."); 
+ 		        $("#alertError").show(); 
+ 		} 
+ 		else{ 
+			    $("#alertError").text("Unknown error while saving.."); 
+	 	 		$("#alertError").show(); 
+ 		} 
+ 		
+	    $("#hidItemIDSave").String(""); 
+	    $("#formItem")[0].reset();
+ 
+   }
+ 		 
+
  	 
  	 // UPDATE==========================================
  	 
 	$(document).on("click", ".btnUpdate", function(event){ 
-	$("#hidIDSave").val($(this).closest("tr").find('#hidupdatebuyerservice').val()); 
- 	$("#FullName").val($(this).closest("tr").find('td:eq(0)').text()); 
-	$("#PhoneNumber").val($(this).closest("tr").find('td:eq(1)').text()); 
-	$("#Email").val($(this).closest("tr").find('td:eq(2)').text()); 
-	$("#Address").val($(this).closest("tr").find('td:eq(3)').text());
-	$("#Birthdate").val($(this).closest("tr").find('td:eq(4)').text()); 
+	
+	$("#hidIDSave").var($(this).closest("tr").find('#hidupdatebuyerservice').val()); 
+ 	$("#FullName").var($(this).closest("tr").find('td:eq(0)').text()); 
+	$("#PhoneNumber").var($(this).closest("tr").find('td:eq(1)').text()); 
+	$("#Email").var($(this).closest("tr").find('td:eq(2)').text()); 
+	$("#Address").var($(this).closest("tr").find('td:eq(3)').text());
+	$("#Birthdate").var($(this).closest("tr").find('td:eq(4)').text()); 
 });
+
+
+
+// REMOVE Button handler(Request Algorithm)=======================================================================================
+
+		$(document).on("click", ".btnRemove", function(event)
+		{ 
+			 $.ajax( 
+ 		 	{ 
+				 url : "BuyerAPI", 
+				 type : "DELETE", 
+		   		 data : "itemID=" + $(this).data("id"),
+ 		 		 dataType : "text", 
+			 	complete : function(response, status) 
+		 		{
+		  			onItemDeleteComplete(response.responseText, status); 
+ 			
+ 	    		}
+ 	  
+ 		}); 
+ 
+	});
+	
+	
+	
+	//Remove Button View (Response Algorithm)-----------------------------------------------------------------------------
+
+		function onItemDeleteComplete(response, status){ 
+			if (status == "success"){ 
+		 		var resultSet = JSON.parse(response); 
+		 		
+ 				if (resultSet.status.trim() == "success"){ 
+		 			 $("#alertSuccess").text("Successfully deleted."); 
+	 		 		 $("#alertSuccess").show(); 
+ 					 $("#divItemsGrid").html(resultSet.data); 
+ 	
+	  			} 
+	  			else if (resultSet.status.trim() == "error"){ 
+ 					$("#alertError").text(resultSet.data); 
+ 					$("#alertError").show(); 
+ 	 		    } 
+ 		
+	 	   } 
+	 	   else if (status == "error"){ 
+ 	  
+ 				$("#alertError").text("Error while deleting."); 
+ 				$("#alertError").show(); 
+ 	  	   } 
+ 	  	   else{ 
+ 				$("#alertError").text("Unknown error while deleting.."); 
+ 				$("#alertError").show(); 
+ 	   	   }
+ 	  
+	}
+
+
  	 
  	 
  	 // CLIENT-MODEL================================================================
  	 function validateItemForm(){ 
 
 	// name
-	if ($("#FullName").val().trim() == ""){ 
+	if ($("#FullName").var().trim() == ""){ 
  	return "Insert Full Name."; 
  	}
  	 
 	// phone number
-	if ($("#PhoneNumber").val().trim() == ""){ 
+	if ($("#PhoneNumber").var().trim() == ""){ 
  	return "Insert Phone Number."; 
  	}
  	 
 	// email
-	if ($("#Email").val().trim() == ""){ 
+	if ($("#Email").var().trim() == ""){ 
  	return "Insert Email."; 
  	} 
 
 	// address
-	if ($("#Address").val().trim() == ""){ 
+	if ($("#Address").var().trim() == ""){ 
  	return "Insert Address."; 
  	}
  	
  	// Birthday
-	if ($("#Birthdate").val().trim() == ""){ 
+	if ($("#Birthdate").var().trim() == ""){ 
  	return "Insert Birth date."; 
  	}
 
