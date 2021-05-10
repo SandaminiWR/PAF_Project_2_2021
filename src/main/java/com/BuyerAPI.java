2f308dfb37a8b12f10d1c1647d5340a74a4c8897
Pasindu Nimsara <pasindunimsara26@gmail.com>
2021-05-10 20:57:20 +0530

    add code

--- src/main/java/com/BuyerAPI.java
+++ src/main/java/com/BuyerAPI.java
@@ -1,7 +1,12 @@
 package com;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/BuyerAPI")
 public class BuyerAPI extends HttpServlet{
@@ -14,6 +19,11 @@ public class BuyerAPI extends HttpServlet{
         
     }
 	
+ // Read Operation
+ 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+ 	{
+ 		//Not Used
+ 	}
 	
 
 }
