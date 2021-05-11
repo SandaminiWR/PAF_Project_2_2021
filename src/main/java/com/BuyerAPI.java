03e8f82f5d369e9794c2d3c08112c127efb8300a
Pasindu Nimsara <pasindunimsara26@gmail.com>
2021-05-11 16:18:13 +0530

    update program

--- src/main/java/com/BuyerAPI.java
+++ src/main/java/com/BuyerAPI.java
@@ -11,86 +11,110 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Servlet implementation class BuyerAPI
+ */
 @WebServlet("/BuyerAPI")
-public class BuyerAPI extends HttpServlet{
+public class BuyerAPI extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
+	
+	
 	Byer itemObj = new Byer();
 	
-    public BuyerAPI(){
-        super();
-        
-    }
 	
- // Read Operation
-protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
- 		//Not Used
- 	}
- 	
- 	
-protected void doPost(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BuyerAPI() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+    
+    
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	
+	
+	
+	
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    
+    	// Insert Operation
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String output = itemObj.insertbuyerservice(request.getParameter("FullName"),
+	 			request.getParameter("PhoneNumber"),
+	 			request.getParameter("Email"),
+	 			request.getParameter("Address"),
+	 			request.getParameter("Birthdate"));
+	 			response.getWriter().write(output);
+	 			
+		
+	}
+	
+	
+	protected void doPut(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
  		
  		
- 			String output = itemObj.insertbuyerservice(request.getParameter("FullName"),
- 			request.getParameter("PhoneNumber"),
- 			request.getParameter("Email"),
- 			request.getParameter("Address"),
- 			request.getParameter("Birthdate"));
- 			response.getWriter().write(output);
- 			}
- 	
- 	
- protected void doPut(HttpServletRequest request, HttpServletResponse response)throws ServletException, IOException{
  		
- 		
- 		
- 			 Map paras = getParasMap(request);
- 			 String output = itemObj.updatebuyerservice(paras.get("hidItemIDSave").toString(),
- 			 paras.get("FullName").toString(),
- 			 paras.get("PhoneNumber").toString(),
- 			 paras.get("Email").toString(),
- 			 paras.get("Address").toString(),
- 			 paras.get("Birthdate").toString());
- 			 response.getWriter().write(output);
- 			 }
- 			 private Map getParasMap(HttpServletRequest request) {
+		 Map paras = getParasMap(request);
+		 String output = itemObj.updatebuyerservice(paras.get("hidItemIDSave").toString(),
+		 paras.get("FullName").toString(),
+		 paras.get("PhoneNumber").toString(),
+		 paras.get("Email").toString(),
+		 paras.get("Address").toString(),
+		 paras.get("Birthdate").toString());
+		 response.getWriter().write(output);
+		 }
+	
+	 private Map getParasMap(HttpServletRequest request) {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	 
+	 
+	 private Map getParasMap(HttpServletRequest request) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	 
+	 
+	 
 
-			protected void doDelete(HttpServletRequest request, HttpServletResponse response)
- 			 throws ServletException, IOException
- 			{
- 			 Map paras = getParasMap(request);
- 			 String output = itemObj.deletebuyerservice(paras.get("ID").toString());
- 			response.getWriter().write(output);
- 			}
-
-			
-			
-			private static Map getParasMap(HttpServletRequest request)
-			{
-				Map<String, String> map = new HashMap<String, String>();
-				try{
-								
-					Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
-					String queryString = scanner.hasNext() ?
-							scanner.useDelimiter("\\A").next() : "";
-					scanner.close();
-					String[] params = queryString.split("&");
-					for (String param : params){ 
-						String[] p = param.split("=");
-						map.put(p[0], p[1]);
-					}
-				}catch (Exception e){
-			 
-				
+		private static Map getParasMap(HttpServletRequest request)
+		{
+			Map<String, String> map = new HashMap<String, String>();
+			try{
+							
+				Scanner scanner = new Scanner(request.getInputStream(), "UTF-8");
+				String queryString = scanner.hasNext() ?
+						scanner.useDelimiter("\\A").next() : "";
+				scanner.close();
+				String[] params = queryString.split("&");
+				for (String param : params){ 
+					String[] p = param.split("=");
+					map.put(p[0], p[1]);
 				}
-				
-				return map;
+			}catch (Exception e){
+		 
+			
 			}
-
-
-	
+			
+			return map;
+		}
+	 
 
 }
---
+++ src/main/webapp/components/Buyers.js
@@ -0,0 +1,186 @@
+
+$(document).ready(function(){
+	
+	//if ($("#alertSuccess").text().trim() == "") {
+		 $("#alertSuccess").hide(); 
+ 	//} 
+ 		 $("#alertError").hide(); 
+});
+
+// SAVE ============================================
+
+$(document).on("click", "#btnSave", function(event){ 
+	// Clear alerts---------------------
+	 $("#alertSuccess").text(""); 
+	 $("#alertSuccess").hide(); 
+ 	 $("#alertError").text(""); 
+ 	 $("#alertError").hide();
+ 	 
+ 	 
+ 	 // Form validation-------------------
+	var status = validateItemForm(); 
+	if (status != true){ 
+ 		
+ 		$("#alertError").text(status); 
+ 		$("#alertError").show(); 
+		
+		 return; 
+	 } 
+	
+	// If valid-------------------------
+ 		//$("#buyerdetails").submit();
+ 		
+ 		var type = ($("#hidItemIDSave").val() == "") ? "POST" : "PUT"; 
+ 		
+		 $.ajax( 
+	     { 
+				 url : "BuyersAPI", 
+ 				 type : type, 
+                 data : $("#buyerdetails").serialize(), 
+                 dataType : "text", 
+                 complete : function(response, status) 
+ 				 {
+ 				 	onItemSaveComplete(response.responseText, status); 
+ 				 } 
+ 		}); 
+  }); 
+
+
+//Save Button View (Response Algorithm)--------------------------------------------------------------------------------------
+
+function onItemSaveComplete(response, status){ 
+ 
+ 			if (status == "success"){ 
+ 	   		 	var resultSet = JSON.parse(response); 
+ 	   	 
+			 	if (resultSet.status.trim() == "success"){ 
+ 
+ 					$("#alertSuccess").text("Successfully saved."); 
+ 					$("#alertSuccess").show(); 
+ 					$("#divItemsGrid").html(resultSet.data);
+ 	 	 	
+ 	 	 	} 
+ 	 	 	else if (resultSet.status.trim() == "error"){ 
+ 	  			
+ 				$("#alertError").text(resultSet.data); 
+ 				$("#alertError").show(); 
+		    } 
+	 
+ 	    } 
+ 	    else if (status == "error"){
+   	     	
+ 			    $("#alertError").text("Error while saving."); 
+ 		        $("#alertError").show(); 
+ 		} 
+ 		else{ 
+			    $("#alertError").text("Unknown error while saving.."); 
+	 	 		$("#alertError").show(); 
+ 		} 
+ 		
+	    $("#hidItemIDSave").String(""); 
+	    $("#formItem")[0].reset();
+ 
+   }
+ 		 
+
+ 	 
+ 	 // UPDATE==========================================
+ 	 
+	$(document).on("click", ".btnUpdate", function(event){ 
+	
+	$("#hidIDSave").var($(this).closest("tr").find('#hidupdatebuyerservice').val()); 
+ 	$("#FullName").var($(this).closest("tr").find('td:eq(0)').text()); 
+	$("#PhoneNumber").var($(this).closest("tr").find('td:eq(1)').text()); 
+	$("#Email").var($(this).closest("tr").find('td:eq(2)').text()); 
+	$("#Address").var($(this).closest("tr").find('td:eq(3)').text());
+	$("#Birthdate").var($(this).closest("tr").find('td:eq(4)').text()); 
+});
+
+
+
+// REMOVE Button handler(Request Algorithm)=======================================================================================
+
+		$(document).on("click", ".btnRemove", function(event)
+		{ 
+			 $.ajax( 
+ 		 	{ 
+				 url : "BuyerAPI", 
+				 type : "DELETE", 
+		   		 data : "itemID=" + $(this).data("id"),
+ 		 		 dataType : "text", 
+			 	complete : function(response, status) 
+		 		{
+		  			onItemDeleteComplete(response.responseText, status); 
+ 			
+ 	    		}
+ 	  
+ 		}); 
+ 
+	});
+	
+	
+	
+	//Remove Button View (Response Algorithm)-----------------------------------------------------------------------------
+
+		function onItemDeleteComplete(response, status){ 
+			if (status == "success"){ 
+		 		var resultSet = JSON.parse(response); 
+		 		
+ 				if (resultSet.status.trim() == "success"){ 
+		 			 $("#alertSuccess").text("Successfully deleted."); 
+	 		 		 $("#alertSuccess").show(); 
+ 					 $("#divItemsGrid").html(resultSet.data); 
+ 	
+	  			} 
+	  			else if (resultSet.status.trim() == "error"){ 
+ 					$("#alertError").text(resultSet.data); 
+ 					$("#alertError").show(); 
+ 	 		    } 
+ 		
+	 	   } 
+	 	   else if (status == "error"){ 
+ 	  
+ 				$("#alertError").text("Error while deleting."); 
+ 				$("#alertError").show(); 
+ 	  	   } 
+ 	  	   else{ 
+ 				$("#alertError").text("Unknown error while deleting.."); 
+ 				$("#alertError").show(); 
+ 	   	   }
+ 	  
+	}
+
+
+ 	 
+ 	 
+ 	 // CLIENT-MODEL================================================================
+ 	 function validateItemForm(){ 
+
+	// name
+	if ($("#FullName").var().trim() == ""){ 
+ 	return "Insert Full Name."; 
+ 	}
+ 	 
+	// phone number
+	if ($("#PhoneNumber").var().trim() == ""){ 
+ 	return "Insert Phone Number."; 
+ 	}
+ 	 
+	// email
+	if ($("#Email").var().trim() == ""){ 
+ 	return "Insert Email."; 
+ 	} 
+
+	// address
+	if ($("#Address").var().trim() == ""){ 
+ 	return "Insert Address."; 
+ 	}
+ 	
+ 	// Birthday
+	if ($("#Birthdate").var().trim() == ""){ 
+ 	return "Insert Birth date."; 
+ 	}
+
+ 
+}
+ 	 
